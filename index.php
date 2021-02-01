70dfdddf925f0876cb992bdd007da65c58b806c7
Mendes Dany <50655070+SykaSeth@users.noreply.github.com>
2021-02-01 19:51:33 +0100

    try php

--- index.php
+++ index.php
@@ -7,9 +7,7 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Ceci est un test</h1>
-    <p>La page s'affiche !</p>
-
+    <h1>Ceci est un test !</h1>
     <form action="" method="get">
         <label for="itworks">Est-ce que le test fonctionne ?</label>
         <input type="radio" name="itworks" id="yes" value="yes">
@@ -19,5 +17,12 @@
 
         <input type="submit" value="Répondre">
     </form>
+    <div>
+        <?php
+        if(isset($_GET['itworks'])):
+            if($_GET['itworks'] == 'yes'): echo '<p class="yes">Ça marche !</p>'; else: echo '<p class="no">Oh mince..</p>'; endif;
+        endif;
+        ?>
+    </div>
 </body>
 </html>
